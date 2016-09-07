dbc2ee968fe9b6c14cc566c49b43830fc7f865de
Ilshidur <nicolas-coutin@outlook.com>
2016-09-07 23:46:31 +0200

    Generator.generate() returns a Promise.

--- src/generator.js
+++ src/generator.js
@@ -29,41 +29,44 @@ class Generator {
     this.stylesheetPath = stylesheetPath;
   }
 
-  async generate () {
-    try {
-      let requestPatch = this.patch;
-      if (!this.patch) {
-        console.log('No patch version. Requesting ...');
-        let patchData = await api.getDatas('Patch', { parameters: { 'api_key': this.apiKey } });
-        console.log(patchData.response.statusCode + ' : ' + patchData.response.statusMessage);
-        requestPatch = patchData.data[0];
+  generate () {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let requestPatch = this.patch;
+        if (!this.patch) {
+          console.log('No patch version. Requesting ...');
+          let patchData = await api.getDatas('Patch', { parameters: { 'api_key': this.apiKey } });
+          console.log(patchData.response.statusCode + ' : ' + patchData.response.statusMessage);
+          requestPatch = patchData.data[0];
+        }
+        console.log('Patch version : ' + requestPatch);
+        const requestsArgs = { region: this.region, apiKey: this.apiKey, requestPatch: requestPatch };
+        const requests = await generateRequests(this.dataType, requestsArgs);
+        console.log('Got ' + requests.length + ' requests to execute. Executing ...');
+        const imageBuffers = await Promise.all(requests);
+        console.log('Requests executed. Saving the images ...');
+        await saveBuffersAsImages(imageBuffers, path.join(this.downloadFolder, this.dataType), (item) => { return { buffer: item.data, name: item.args.name }; } );
+        console.log(imageBuffers.length + ' images downloaded !');
+        await generateSpriteSheet({
+          src: [path.join(this.downloadFolder, this.dataType, '*.png')],
+          spritePath: this.spritePath,
+          stylesheet: this.stylesheetFormat,
+          stylesheetPath: this.stylesheetPath
+        }, {
+          compressionLevel: 9
+        });
+        console.log('Sprite generated !');
+        await compressImages({
+          src: [this.spritePath],
+          out: this.finalSpritesheetFolder
+        }, {
+          speed: 1
+        });
+        console.log('Compressed images !');
+        resolve();
+      } catch (e) {
+        reject(e);
       }
-      console.log('Patch version : ' + requestPatch);
-      const requestsArgs = { region: this.region, apiKey: this.apiKey, requestPatch: requestPatch };
-      const requests = await generateRequests(this.dataType, requestsArgs);
-      console.log('Got ' + requests.length + ' requests to execute. Executing ...');
-      const imageBuffers = await Promise.all(requests);
-      console.log('Requests executed. Saving the images ...');
-      await saveBuffersAsImages(imageBuffers, path.join(this.downloadFolder, this.dataType), (item) => { return { buffer: item.data, name: item.args.name }; } );
-      console.log(imageBuffers.length + ' images downloaded !');
-      await generateSpriteSheet({
-        src: [path.join(this.downloadFolder, this.dataType, '*.png')],
-        spritePath: this.spritePath,
-        stylesheet: this.stylesheetFormat,
-        stylesheetPath: this.stylesheetPath
-      }, {
-        compressionLevel: 9
-      });
-      console.log('Sprite generated !');
-      await compressImages({
-        src: [this.spritePath],
-        out: this.finalSpritesheetFolder
-      }, {
-        speed: 1
-      });
-      console.log('Compressed images !');
-    } catch (e) {
-      throw e;
-    }
+    });
   }
 }
